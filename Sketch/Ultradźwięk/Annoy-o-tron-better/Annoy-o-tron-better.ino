efb76e6784069014440e00c932dab7ced2b8ab68
Damian Herman <dkpolan3@gmail.com>
2015-01-16 17:25:10 +0100

    annoy-o-tron

--- Sketch/Ultradźwięk/Annoy-o-tron-better/Annoy-o-tron-better.ino
+++ Sketch/Ultradźwięk/Annoy-o-tron-better/Annoy-o-tron-better.ino
@@ -1,4 +1,4 @@
-int leds[] = {12,11}; //tablica z ledami - odwołujemy się za pomocą leds[numerIndeksu]
+int speaker = 7;
 int echo = 9;
 int trigger = 8;
 void setup() {
@@ -20,15 +20,8 @@ void loop() {
   digitalWrite(trigger, LOW);
   duration = pulseIn(echo, HIGH);
   distance = duration / 58;
-  if(distance < 30)
-  {
-    digitalWrite(leds[0], LOW);
-    digitalWrite(leds[1], HIGH);
-  }else
-  {
-    digitalWrite(leds[0], HIGH);
-    digitalWrite(leds[1], LOW);
-  }
+  int out = (distance * 255) / 190;
+  analogWrite(out, 7);
   Serial.println(distance);
-  delay(100);
+   
 }
