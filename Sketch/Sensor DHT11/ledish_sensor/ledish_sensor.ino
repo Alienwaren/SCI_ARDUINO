a80415a9dfd574fdedd298ff3e2508606648bc80
Damian Herman <dkpolan3@gmail.com>
2015-02-25 16:38:11 +0100

    DHT11

---
+++ Sketch/Sensor DHT11/ledish_sensor/ledish_sensor.ino
@@ -0,0 +1,13 @@
+int ledPins[] = {13,12,11};
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0; i < 3; ++i)
+  {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
