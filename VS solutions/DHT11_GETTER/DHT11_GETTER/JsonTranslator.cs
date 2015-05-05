45fbb5823724f2361f44fae9a821a165e8673c06
Damian Herman <dkpolan3@gmail.com>
2015-05-05 12:48:15 +0200

    C# sensor getter solution TODO: make puishing to thingspeak

---
+++ VS solutions/DHT11_GETTER/DHT11_GETTER/JsonTranslator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows;
+namespace DHT11_GETTER
+{
+    public class JsonTranslator
+    {
+        public JsonTranslator()
+        {
+
+        }
+        public SensorData serializeJson(string jsonString)
+        {
+            SensorData data = new SensorData();
+            int valueNumber = 0;
+            JsonTextReader reader = new JsonTextReader(new StringReader(jsonString));
+            while(reader.Read())
+            {
+                if (reader.TokenType == JsonToken.Integer && valueNumber == 0)
+                {
+                    data.temperatureValue = int.Parse(reader.Value.ToString());
+                    valueNumber++;
+                }else if(reader.TokenType == JsonToken.Integer && valueNumber == 1)
+                {
+                    data.humidityTemperature = int.Parse(reader.Value.ToString());
+                }
+            }
+            return data;
+        }
+    }
+}
