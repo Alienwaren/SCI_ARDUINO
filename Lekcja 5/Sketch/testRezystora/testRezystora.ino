34ca67a00e8fc45e468df96d305de7c723db1dd2
Damian Herman <netrunnerproject@outlook.com>
2016-03-01 19:08:41 +0100

    Testy rezystoróW

---
+++ Lekcja 5/Sketch/testRezystora/testRezystora.ino
@@ -0,0 +1,14 @@
+int led = 12; ///nasza diodka led
+///Rezystor podłączony do LEDu zmiejsza jego jasność (ilość prądu która prze niego przepływa), 
+///co wynika z prawa Ohma:
+///R = U/I
+void setup() 
+{
+  pinMode(led, OUTPUT); //ustawiamy led na tryb wyjścia
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
